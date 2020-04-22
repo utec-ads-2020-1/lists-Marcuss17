a2bacf544069537000c80690674cd449112f2309
Marcuss17 <eduardo.medina@utec.edu.pe>
2020-04-21 20:22:27 -0500

    Almost done with forward

--- structures/lists/forward.h
+++ structures/lists/forward.h
@@ -7,104 +7,130 @@
 // TODO: Implement all methods
 template <typename T>
 class ForwardList : public List<T> {
-    public:
-        ForwardList() : List<T>(){};
-
-        T front(){
-		return this.head->data;
-	};
+	public:
+	ForwardList() : List<T>(){};
+	
+		T front(){
+		return this->head->data;
+		};
 
         T back(){
-		auto aux = this.head;
-		while(aux->next != nullptr){
-			aux = aux->next;
-		}
-		return aux->data;
-	};
+			auto aux = this->head;
+			while(aux->next != nullptr){
+				aux = aux->next;
+			}
+			return aux->data;
+		};
 
         void push_front(T element){
-		Node<T>* newNode = new Node<T>();
-		newNode->data = element;
-		newNode->next = this.head;
-		this.head = newNode;
-		this.nodes++;
-	};
+			Node<T>* newNode = new Node<T>();
+			newNode->data = element;
+			newNode->next = this->head;
+			this->head = newNode;
+			this->nodes++;
+		};
 
         void push_back(T element){
-		Node<T>* newNode = new Node<T>();
-		newNode->data = element;
-		newNode->next = nullptr;
-		if(this.head == nullptr){
-			this.head = newNode;
-		}else{
-			auto aux = this.head;
-			while(aux->next != nullptr){
-				aux = aux->next;	
-			}
-			aux->next = newNode;
-		}	
-		this.nodes++;
-	};
+			Node<T>* newNode = new Node<T>();
+			newNode->data = element;
+			newNode->next = nullptr;
+			if(this->head == nullptr){
+				this->head = newNode;
+			}else{
+				auto aux = this->head;
+				while(aux->next != nullptr){
+					aux = aux->next;	
+					}
+				aux->next = newNode;
+			}	
+			this->nodes++;
+		};
 
         void pop_front(){
-		auto aux = this.head;
-		head = head->next;
-		delete aux;	
-		this.nodes--;
-	}
+			auto aux = this->head;
+			this->head = this->head->next;
+			delete aux;
+			this->nodes--;
+		}
 
         void pop_back(){
-		auto aux = this.head;
-		while(aux->next != nullptr){
-			aux = aux->next;
-		}
-		delete aux;
-		aux = nullptr;
-		this.nodes--;
-	};
+			auto aux = this->head;
+			Node<T>* current;
+
+			while(aux->next != nullptr){
+				current = aux;
+				aux = aux->next;
+			}
+
+			current->next= nullptr;
+			cout << current->data << endl;
+			delete aux;
+			
+			this->nodes--;
+		};
 	
         T operator[](int index){
-		int ind = 0;
-		auto aux = this.head;
-		while(ind != index){
-			aux = aux->next;	
-			ind++;
-		}
-		return aux->data;
-	};
+			int ind = 0;
+			auto aux = this->head;
+			while(ind != index){
+				aux = aux->next;	
+				ind++;
+			}
+			return aux->data;
+		};
 	
         bool empty(){
-		return this.head==nullptr;
-	};
+			return this->nodes == 0;
+		};
+
         int size(){
-		return this.nodes;
-	};
+			return this->nodes;
+		};
+
         void clear(){
-		auto aux = this.head;
-		while(aux->next != nullptr){
-			auto aux2 = aux;
-			delete aux2;
-			aux = aux->next;
-			
+			auto aux = this->head;
+			while(aux->next != nullptr){
+				auto aux2 = aux;
+				delete aux2;
+				aux = aux->next;	
+			}
+			this->nodes = 0;
 		}
-		this.nodes = 0;
-	}
+
         void sort(){
-		auto aux = this.head;
-		while(aux -> next != nullptr){
-			auto aux2 = aux->next;
-			while(aux2 ->next != nullptr){
-				if(aux2 -> data < aux -> data){
-					swap(aux2->data,aux->data);
+			auto aux = this->head;
+			for(int i = 0; i <= size()-2; i++){
+				auto aux2 = aux->next;
+				for(int j = 1; j <= size()-1; j++){
+					if(aux-> data > aux2->data){
+						swap(aux->data, aux2->data);
+					}
 				}
-			} 
-		}		
-	}
-        void reverse();
+				aux = aux->next;
+			}	
+		}
+		
+		void reverse(){
+			for(int i = 0; i < size()-1; i++){
+				auto aux = this->head;
+				for(int j = 0; j < size()-1-i; j++){
+					swap(aux->data, aux->next->data);
+					aux = aux->next;
+				}
+			}
+		}
 	
 
-        ForwardIterator<T> begin();
-	ForwardIterator<T> end();
+    	ForwardIterator<T> begin(){
+			return this->head;
+		}
+		ForwardIterator<T> end(){
+			auto aux = this->head;
+			while(aux->next != nullptr){
+				aux = aux->next;
+			}
+			return aux;
+		}
 
         string name() {
             return "Forward List";
@@ -120,7 +146,15 @@ class ForwardList : public List<T> {
          * any element: they are transferred, no matter whether x is an lvalue or an rvalue, 
          * or whether the value_type supports move-construction or not.
         */
-        void merge(ForwardList<T>&);
+        void merge(ForwardList<T>& secondForward){
+			auto secondBeg = secondForward->head;
+			auto aux = this->head;
+			while(aux->next != nullptr){
+				aux = aux->next;
+			}
+
+			aux->next = secondBeg;
+		};
 };
 
 #endif
