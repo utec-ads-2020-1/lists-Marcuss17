3cb614f0a3e7570e326e8ca940edf765107a07bb
Marcuss17 <eduardo.medina@utec.edu.pe>
2020-04-25 02:11:40 -0500

    Dont know why but when uploaded with vscode the structure was broken, so had to modify it in vim

--- structures/lists/linked.h
+++ structures/lists/linked.h
@@ -26,7 +26,7 @@ class LinkedList : public List<T> {
 
         void push_front(T element){
             Node<T>* newNode = new Node<T>();
-			newNode->data = element;
+	    newNode->data = element;
             newNode->prev = nullptr;
             if(this->head == nullptr && this->tail == nullptr){
                 newNode->next = nullptr;
@@ -37,12 +37,12 @@ class LinkedList : public List<T> {
                 newNode->next = aux;
             }
             this->head = newNode;
-			this->nodes++;
+	    this->nodes++;
         }
 
         void push_back(T element){
             Node<T>* newNode = new Node<T>();
-			newNode->data = element;
+	    newNode->data = element;
             newNode->next = nullptr;
             if(this->head == nullptr && this->tail == nullptr){
                 newNode->prev = nullptr;
@@ -70,7 +70,6 @@ class LinkedList : public List<T> {
             this->tail->next = nullptr;
             delete aux;
             this->nodes--;
-
         }
 
         T operator[](int index){
@@ -81,12 +80,12 @@ class LinkedList : public List<T> {
                 throw string("Error in the index, check it!");
             }
             int ind = 0;
-			auto aux = this->head;
-			while(ind != index){
-				aux = aux->next;	
-				ind++;
-			}
-			return aux->data;
+	    auto aux = this->head;
+	    while(ind != index){
+		aux = aux->next;	
+		ind++;
+	    }
+	    return aux->data;
         }
 
         bool empty(){
@@ -99,46 +98,47 @@ class LinkedList : public List<T> {
 
         void clear(){
             while(this->head != nullptr){
-				auto aux = this->head;
-				this->head= this->head->next;
-				delete aux;	
-			}
+		auto aux = this->head;
+		this->head= this->head->next;
+		delete aux;	
+	    }
 
-			this->tail = this->head;
-			this->nodes = 0;
+	    this->tail = this->head;
+	    this->nodes = 0;
         }
         
         void sort(){
-			auto current = this->tail->prev;
-			auto aux = this->head;
-			auto aux2 = this->head;
-			while(aux != current){
-				aux2 = this->head;
-				while(aux2 != this->tail){
-					if(aux2->data > aux2->next->data){
-						swap(aux2->data,aux2->next->data);
-					}
-					aux2 = aux2->next;
-				}
-				aux = aux->next;
-			}
+	    auto current = this->tail->prev;
+	    auto aux = this->head;
+	    auto aux2 = this->head;
+	    while(aux != current){
+		aux2 = this->head;
+		while(aux2 != this->tail){
+		    if(aux2->data > aux2->next->data){
+		    swap(aux2->data,aux2->next->data);
+		}
+		aux2 = aux2->next;
+	    }
+	    aux = aux->next;
+	    }
         }
 
         void reverse(){
             for(int i = 0; i < size()-1; i++){
-				auto aux = this->head;
-				for(int j = 0; j < size()-1-i; j++){
-					swap(aux->data, aux->next->data);
-					aux = aux->next;
-				}
-			}
+		auto aux = this->head;
+		for(int j = 0; j < size()-1-i; j++){
+		    swap(aux->data, aux->next->data);
+		    aux = aux->next;
+		}
+	    }
         }
 
         BidirectionalIterator<T> begin(){
             BidirectionalIterator<T> it(this->head);
             return it;
         }
-	    BidirectionalIterator<T> end(){
+
+	BidirectionalIterator<T> end(){
             BidirectionalIterator<T> it(this->tail->next);
             return it;
         }
